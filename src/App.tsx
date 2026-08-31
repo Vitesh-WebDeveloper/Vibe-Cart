e6557defa316079a9f825b674f58c9660e334eb3
Pulluri Sai Vitesh <saivitesh4@gmail.com>
2026-08-31 21:42:44 +0530

    initial project files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
 
     <div className="p-10 text-red-500 text-center font-bold text-2xl bg-indigo-300">
-  Tailwind is working!kkk
+  Tailwind is working!kkkK
 </div>
 
   )
