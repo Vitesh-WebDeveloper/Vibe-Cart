b7b527bcdca440c2480931267f5a443647d73f21
Pulluri Sai Vitesh <saivitesh4@gmail.com>
2026-08-31 21:33:07 +0530

    Started with new project VibeCart

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+
+    <div className="p-10 text-red-500 text-center font-bold text-2xl bg-indigo-300">
+  Tailwind is working!kkk
+</div>
+
+  )
+}
+
+export default App
